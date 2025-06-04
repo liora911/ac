efc3146ac24784995be80b8815c3e48dc9542c87
krislordemaze <yarin.mimon@emaze.com>
2025-06-04 16:10:32 +0300

    remove phone from footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="w-full px-4 py-6  text-center text-sm text-muted-foreground border-t border-gray-200 dark:border-gray-800">
-      <p>להזמנות: 050-8228404 avshalom@iyar.org.il</p>
+      <p>avshalom@iyar.org.il: להזמנות</p>
     </footer>
   );
 }
