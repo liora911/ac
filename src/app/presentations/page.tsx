db8685712e7e9763a1ee982753e733976b492b2e
krislordemaze <yarin.mimon@emaze.com>
2025-06-04 16:03:54 +0300

    footer,lectures adding more data, bgc changing

--- src/app/presentations/page.tsx
+++ src/app/presentations/page.tsx
@@ -1,7 +1,7 @@
 import Presentations from "@/components/Presentations/presentations";
 
 const Page = () => (
-  <main className="flex min-h-screen flex-col items-center bg-gray-50 py-8 sm:py-12">
+  <main className="flex min-h-screen flex-col items-center py-8 sm:py-12">
     <Presentations />
   </main>
 );
