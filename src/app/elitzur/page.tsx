674a790857378fa4744e4583c48aed2cae627db9
krislordemaze <yarin.mimon@emaze.com>
2025-06-29 21:47:31 +0300

    Fix useSearchParams Suspense boundary issue

--- src/app/elitzur/page.tsx
+++ src/app/elitzur/page.tsx
@@ -1,9 +1,12 @@
 import LoginForm from "@/components/Login/login";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
