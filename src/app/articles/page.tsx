031fe6e2c92154ae681f0e1502b2f9a2873e2366
krislordemaze <yarin.mimon@emaze.com>
2025-10-15 22:21:25 +0300

    auth article create

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -4,9 +4,17 @@ import React from "react";
 import Link from "next/link";
 import ArticlesList from "@/components/Articles/ArticlesList";
 import { useTranslation } from "@/contexts/Translation/translation.context";
+import { useSession } from "next-auth/react";
+import { ALLOWED_EMAILS } from "@/constants/auth";
 
 export default function ArticlesPage() {
   const { t } = useTranslation();
+  const { data: session } = useSession();
+
+  const isAuthorized =
+    session?.user?.email &&
+    ALLOWED_EMAILS.includes(session.user.email.toLowerCase());
+
   return (
     <div className="min-h-screen bg-gray-50">
       {}
@@ -19,13 +27,15 @@ export default function ArticlesPage() {
             <p className="text-xl text-gray-600 max-w-3xl mx-auto">
               {t("articlesPage.description")}
             </p>
-            <div className="mt-6">
-              <Link href="/articles/create" passHref>
-                <button className="bg-blue-600 text-white px-6 py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors duration-200">
-                  {t("articlesPage.createArticleButton")}
-                </button>
-              </Link>
-            </div>
+            {isAuthorized && (
+              <div className="mt-6">
+                <Link href="/articles/create" passHref>
+                  <button className="bg-blue-600 text-white px-6 py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors duration-200">
+                    {t("articlesPage.createArticleButton")}
+                  </button>
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </div>
