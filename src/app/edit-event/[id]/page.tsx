46e17be5bab783c3a16be58215ace36b14784899
krislordemaze <yarin.mimon@emaze.com>
2025-11-05 13:55:51 +0200

    fix editevent params

--- src/app/edit-event/[id]/page.tsx
+++ src/app/edit-event/[id]/page.tsx
@@ -1,15 +1,16 @@
 import EditEventForm from "@/components/EditEvent/edit_event";
 
 interface EditEventPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
-export default function EditEventPage({ params }: EditEventPageProps) {
+export default async function EditEventPage({ params }: EditEventPageProps) {
+  const { id } = await params;
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 py-10 px-4">
-      <EditEventForm eventId={params.id} />
+      <EditEventForm eventId={id} />
     </div>
   );
 }
