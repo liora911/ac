a08c52b5eb75919170bb42c957635b5c0834a9e9
krislordemaze <yarin.mimon@emaze.com>
2026-01-11 13:21:34 +0200

    accesibility improve for keyboard

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -124,7 +124,7 @@ export default function Header() {
       {/* Skip to main content link for keyboard navigation */}
       <a
         href="#main-content"
-        className="sr-only focus:not-sr-only focus:absolute focus:top-2 focus:left-2 focus:z-[60] focus:px-4 focus:py-2 focus:bg-blue-600 focus:text-white focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+        className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-[100] focus:px-4 focus:py-2 focus:bg-blue-600 focus:text-white focus:rounded-lg focus:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
       >
         {t("header.skipToContent")}
       </a>
