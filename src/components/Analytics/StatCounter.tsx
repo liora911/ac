0f254d7a3426505f27e9d991c65e63908d6bf20c
krislordemaze <yarin.mimon@emaze.com>
2026-02-13 19:19:05 +0200

    fix/statcounter

--- src/components/Analytics/StatCounter.tsx
+++ src/components/Analytics/StatCounter.tsx
@@ -1,44 +1,53 @@
 "use client";
 
-import Script from "next/script";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+
+declare global {
+  interface Window {
+    sc_project?: number;
+    sc_invisible?: number;
+    sc_security?: string;
+  }
+}
 
 export default function StatCounter() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.sc_project = 13202600;
+    window.sc_invisible = 1;
+    window.sc_security = "ec5154da";
+
+    const existingScript = document.getElementById("statcounter-script");
+    if (existingScript) {
+      existingScript.remove();
+    }
+
+    const script = document.createElement("script");
+    script.src = "https://www.statcounter.com/counter/counter.js";
+    script.async = true;
+    script.id = "statcounter-script";
+    document.body.appendChild(script);
+  }, [pathname]);
+
   return (
-    <>
-      <Script
-        id="statcounter-config"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var sc_project=13202600;
-            var sc_invisible=1;
-            var sc_security="ec5154da";
-          `,
-        }}
-      />
-      <Script
-        id="statcounter"
-        strategy="afterInteractive"
-        src="https://www.statcounter.com/counter/counter.js"
-        async
-      />
-      <noscript>
-        <div className="statcounter">
-          <a
-            title="hit counter"
-            href="https://statcounter.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="statcounter"
-              src="https://c.statcounter.com/13202600/0/ec5154da/1/"
-              alt="hit counter"
-              referrerPolicy="no-referrer-when-downgrade"
-            />
-          </a>
-        </div>
-      </noscript>
-    </>
+    <noscript>
+      <div className="statcounter">
+        <a
+          title="hit counter"
+          href="https://statcounter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="statcounter"
+            src="https://c.statcounter.com/13202600/0/ec5154da/1/"
+            alt="hit counter"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </a>
+      </div>
+    </noscript>
   );
 }
