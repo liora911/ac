d5a8dbb523b0a38e11d2c2146bca04ca8dd6364c
krislordemaze <yarin.mimon@emaze.com>
2026-03-11 13:29:31 +0200

    next config proxy whitelist yg images

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "*.blob.vercel-storage.com",
       },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
     ],
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
