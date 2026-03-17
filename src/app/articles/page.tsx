ad3d0a5debfe25f4b161639f5d451ffcb18b01b2
krislordemaze <yarin.mimon@emaze.com>
2026-03-17 13:52:52 +0200

    fix/lecture page banner replace

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -24,7 +24,7 @@ function ArticlesPageContent() {
   const categoryId = searchParams.get('c');
   const bannerSrc =
     categoryId === "cmldlqdpq0001l804wu8113br"
-      ? "https://vo7mgluzosvw8wff.public.blob.vercel-storage.com/1770451469610-strmst.png"
+      ? "https://vo7mgluzosvw8wff.public.blob.vercel-storage.com/1773748316734-05g5af.png"
       : "/gstudioArticles.jpg";
 
   return (
